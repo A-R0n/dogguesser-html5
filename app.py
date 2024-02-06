9cc2c5f6b6cdc1846ff338a04a937ea7e8c85d35
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2024-02-06 17:22:05 -0600

    test using boto3

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import boto3
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/sign_s3/')
+def sign_s3():
+  S3_BUCKET = 'dogguesser'
+
+  file_name = request.args.get('file_name')
+  file_type = request.args.get('file_type')
+
+  s3 = boto3.client('s3')
+
+  presigned_post = s3.generate_presigned_post(
+    Bucket = S3_BUCKET,
+    Key = file_name,
+    Fields = {"acl": "public-read", "Content-Type": file_type},
+    Conditions = [
+      {"acl": "public-read"},
+      {"Content-Type": file_type}
+    ],
+    ExpiresIn = 3600
+  )
+
+  return json.dumps({
+    'data': presigned_post,
+    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+  })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
