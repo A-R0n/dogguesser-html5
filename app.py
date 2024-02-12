ef262c5b2d92cde8c91c4ff856ae8025c09c241b
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2024-02-12 10:47:33 -0600

    debug false

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def change_label():
     # return message
 
 if __name__ == "__main__":
-      application.run(host='0.0.0.0', port='8080', debug=True)
+      application.run(host='0.0.0.0', port='8080')
