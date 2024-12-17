279b4282fcf502ddda69fdf7cf66fe8ec4605b42
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2024-12-17 13:06:24 -0600

    open file on upload for s3 to get content

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import logging
 from guess_dog import guess_dog
 from upload_file_to_s3 import upload_file_to_s3
-from threading import Thread
 import toml
 import os
-from multiprocessing import Process
-import io
-import shutil
+
 
 logging.basicConfig(level=logging.WARNING)
 application = Flask(__name__)
@@ -16,24 +13,14 @@ application.wsgi_app = ProxyFix(
     application.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
-# p ='config.toml'
-p ="/home/ec2-user/app/config.toml"
+p ='config.toml'
+# p ="/home/ec2-user/app/config.toml"
 with open(p, "r") as f:
     config = toml.load(f)
 
 application.config.from_object(config)
 application.config.secret_key = os.urandom(24)
 
-
-def copy_stream(input_stream):
-    buffer = io.BytesIO()  # Or io.StringIO() for text data
-    shutil.copyfileobj(input_stream, buffer)
-    buffer.seek(0)  # Reset the buffer position to the beginning
-    return buffer
-
-
-def wrapper(func, arg, queue):
-     queue.put(func(arg))
 
 @application.route("/")
 def index():
@@ -43,27 +30,16 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in config['app']['allowed_extensions']
 
-def provide_dog_guess(guess: dict):
-    return jsonify(guess)
-
-def runInParallel(*fns):
-    proc = []
-    for fn in fns:
-        p = Process(target=fn)
-        p.start()
-        proc.append(p)
-    for p in proc:
-        p.join()
 
 @application.route("/change_label", methods=["POST"])
 def change_label():
+    
     if 'user_file' not in request.files:
         application.logger.warning(f'uh oh')
         flash('No user_file key in request.files')
         return redirect(url_for('index'))
+    
     file = request.files['user_file']
-    filename = file.filename
-    file.save(filename)
 
     if file.filename == '':
         application.logger.warning(f'oh no')
@@ -72,20 +48,12 @@ def change_label():
 
 
     if file and allowed_file(file.filename):
-        with open(f'{filename}', 'rb') as input_file:
-            copy = copy_stream(input_file)
-
-        file2 = copy.read()
-        thread = Thread(target=upload_file_to_s3, args=(file2,))
         dog_guessed = guess_dog(file)
-        print(f'starting thread')
-        thread.start()
-        print(f'returning json')
+        upload_file_to_s3(file)
         return jsonify({"guess": dog_guessed, "visibility": "visible"})
     return jsonify({"guess": "None", "visibility": "visible"})
 
 
-
 if __name__ == "__main__":
-    application.run(host=config['app']['host'], port=config['app']['port'])
-    # application.run(host=config['app']['host'], port=config['app']['port'], debug=True)
+    # application.run(host=config['app']['host'], port=config['app']['port'])
+    application.run(host=config['app']['host'], port=config['app']['port'], debug=True)
--- upload_file_to_s3.py
+++ upload_file_to_s3.py
@@ -2,15 +2,13 @@ import time
 import boto3
 # from werkzeug.utils import secure_filename
 from boto3.s3.transfer import TransferConfig
-import _io
-import io
 
 
 def get_time() -> str:
     return str(time.time_ns())
 
-def get_file_ext(file: _io.BufferedReader) -> str:
-    return str(file.name).split(".")[1]
+def get_file_ext(file) -> str:
+    return str(file.filename).split(".")[1]
 
 def set_new_file_name(curr_time: str, file_ext: str) -> str:
     return curr_time + "." + file_ext
@@ -18,26 +16,26 @@ def set_new_file_name(curr_time: str, file_ext: str) -> str:
 def set_config() -> TransferConfig:
     return TransferConfig(multipart_threshold=1024*250, max_concurrency=10, multipart_chunksize=1024*250, use_threads=True)
 
-def upload_file_to_s3(file: _io.BufferedReader) -> str:
+def upload_file_to_s3(file) -> str:
     curr_time = get_time()
-    new_file_name = set_new_file_name(curr_time, 'jpg')
+    file_ext = get_file_ext(file)
+    new_file_name = set_new_file_name(curr_time, file_ext)
     # we shouldnt need to santize the filname because we define acceptable file types before upload
     # filename = secure_filename(file.filename)
     config = set_config()
     s3 = boto3.client('s3')
     try:
         print(f'attemtpting upload...')
-        # import time
-        # time.sleep(5)
-        s3.upload_fileobj(
-            io.BytesIO(file),
-            'dogguesser',
-            new_file_name,
-            ExtraArgs={
-                "ACL": "public-read",
-                "ContentType": f'image/.jpg'
-            },
-            Config=config
-        )
+        with open(file.filename, 'rb') as img:
+            s3.upload_fileobj(
+                img,
+                'dogguesser',
+                new_file_name,
+                ExtraArgs={
+                    "ACL": "public-read",
+                    "ContentType": file.content_type
+                },
+                Config=config
+            )
     except Exception as e:
         print(f'file not uploaded: {e}')
