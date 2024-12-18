b560f1685faf5612c320daab461f1b9c756bf5f4
A-R0n <aestesc@gmail.com>
2024-12-18 17:58:04 +0000

    without the craziness

--- read_file_from_s3.py
+++ read_file_from_s3.py
@@ -5,8 +5,4 @@ def read_file_from_s3(file_name):
     s3 = boto3.client('s3')
     obj = s3.get_object(Bucket='dogguesser', Key=file_name)
     data = obj['Body'].read()
-<<<<<<< HEAD
     return data
-=======
-    return data
->>>>>>> 4abef79708a977f18290dfd688175bbefe2f1ef9
